748f5ed7c3a07fd861e684afcd50913b94f6be05
Jordan Lang <jordandlang.sci@gmail.com>
2025-12-17 19:40:25 +0100

    Forest config v2

---
+++ forest_CRABConfig_151X_2025PbPb_UPC_MITHIG.py
@@ -0,0 +1,60 @@
+# 2025 PbPb UPC
+# CMSSW_15_1_0_patch3
+# HIForward[0-??]
+
+from CRABClient.UserUtilities import config
+from CRABClient.UserUtilities import getUsername
+username = getUsername()
+
+###############################################################################
+# INPUT/OUTPUT SETTINGS
+
+pd = '8'
+run = '399658-399662'
+jobTag = 'PbPbUPC_HIForward' + pd + '_' + run
+cmsswConfig = 'forest_CMSSWConfig_Run3_151X_2025PbPb_MITUPCStudies_v2.py'
+
+isOnDAS = True
+# If isOnDAS == True, use these inputs:
+input = '/HIForward' + pd + '/HIRun2025A-PromptReco-v1/MINIAOD'
+inputDatabase = 'global'
+# Otherwise, use a filelist as input:
+inputFilelist = 'filelist_HIForward' + pd + '_' + run + '.txt'
+
+output = '/store/group/phys_heavyions/' + username + '/Run3_PbPbUPC/Forest_2025_PromptReco/'
+outputServer = 'T2_CH_CERN'
+
+###############################################################################
+
+config = config()
+
+config.General.requestName = jobTag
+config.General.workArea = 'CrabWorkArea'
+config.General.transferOutputs = True
+
+config.JobType.psetName = cmsswConfig
+config.JobType.pluginName = 'Analysis'
+config.JobType.maxMemoryMB = 3000
+config.JobType.pyCfgParams = ['noprint']
+config.JobType.allowUndistributedCMSSW = True
+
+if isOnDAS :
+    config.Data.inputDataset = input
+    config.Data.inputDBS = inputDatabase
+    config.Data.lumiMask = '/afs/cern.ch/user/j/jdlang/public/2025PbPb_DCS_20251118.json'
+    config.Data.runRange = run
+    config.Data.splitting = 'EventAwareLumiBased'
+    config.Data.unitsPerJob = 500
+    config.Data.totalUnits = -1
+else :
+    config.Data.outputPrimaryDataset = jobTag
+    config.Data.userInputFiles = open(inputFilelist).readlines()
+    config.Data.splitting = 'FileBased'
+    config.Data.unitsPerJob = 1
+    config.Data.totalUnits = -1
+
+config.Data.outLFNDirBase = output
+config.Data.publication = False
+config.Data.allowNonValidInputDataset = True
+
+config.Site.storageSite = outputServer
