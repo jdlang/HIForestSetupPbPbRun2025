e9bddda69831f1a277b24db34b41a27b8ed1ed0e
jdlang <1473926+jdlang@users.noreply.github.com>
2026-06-18 13:17:06 -0400

    Updated to latest golden json

--- forest_CRABConfig_161X_2026PbPb_PromptReco_UPC_MITHIG.py
+++ forest_CRABConfig_161X_2026PbPb_PromptReco_UPC_MITHIG.py
@@ -10,7 +10,7 @@ username = getUsername()
 # INPUT/OUTPUT SETTINGS
 
 pd = 'HIForward0'
-jobTag = '2026PbPbUPC_PromptReco_20260610Forest_' + pd
+jobTag = '2026PbPbUPC_PromptReco_20260618Forest_404337-404509_' + pd
 cmsswConfig = 'forest_CMSSWConfig_Run3_161X_2026PbPb_PromptReco_UPC_MITHIG.py'
 
 inputDAS = '/' + pd + '/HIRun2026A-PromptReco-v1/MINIAOD'
@@ -37,8 +37,8 @@ config.JobType.allowUndistributedCMSSW = True
 
 config.Data.inputDataset = inputDAS
 config.Data.inputDBS = inputDatabase
-config.Data.lumiMask = '/eos/user/c/cmsdqm/www/CAF/certification/Collisions26HI/DCSOnly_JSONS/dailyDCSOnlyJSON/Collisions26HI_5p36TeV_Latest.json'
-config.Data.runRange = '404338-404656'
+config.Data.lumiMask = '/eos/user/c/cmsdqm/www/CAF/certification/Collisions26HI/Collisions26HI_5p36TeV_404337_404509_golden.json'
+config.Data.runRange = '404337-404509'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 5
 config.Data.totalUnits = -1
